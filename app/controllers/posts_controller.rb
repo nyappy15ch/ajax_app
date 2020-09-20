9d957533e4a8f70fa4126389ccae8c2a0549e8ca
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-09-20 14:37:32 +0900

    Revert "Merge pull request #1 from nyappy15ch/既読機能実装"
    This reverts commit 75b5efc0aecc8b583221b84a57d34c89a4c9b777, reversing
    changes made to d8d5075f721bb8971bbf9f787861e7994cc7d7e7.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,17 +7,4 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-
-  def checked
-    post = Post.find(params[:id])
-    if post.checked
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
-    item = Post.find(params[:id])
-    render json: { post: item }#itemを代入しているpost変数がレスポンスで使用できるようになる
-    #レスポンスをjson形式で返すためにrenderを使用している。
-  end
-  
 end
